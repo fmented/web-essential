4cce46fe342c88da7377f018658be1710ec5ded0
fmented <fmented@gmail.com>
2021-02-04 15:33:43 +0700

    Update precache-manifest.9cd20cc56abb0cdadd94adaccc80f298.js

--- precache-manifest.9cd20cc56abb0cdadd94adaccc80f298.js
+++ precache-manifest.9cd20cc56abb0cdadd94adaccc80f298.js
@@ -1,66 +1,66 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "d10d62a4c42dce917eb26e10fba6c90e",
-    "url": "/browserconfig.xml"
+    "url": "./browserconfig.xml"
   },
   {
     "revision": "9ba644d1fac3d70306a1",
-    "url": "/css/app.86181cdf.css"
+    "url": "./css/app.86181cdf.css"
   },
   {
     "revision": "cde080bdb24dd3123f452031126feec8",
-    "url": "/favicon-128x128.png"
+    "url": "./favicon-128x128.png"
   },
   {
     "revision": "5d55354a8dab447d18e42d4fd510d604",
-    "url": "/favicon-16x16.png"
+    "url": "./favicon-16x16.png"
   },
   {
     "revision": "a5f35c88ebd25df24db42fb64040eafc",
-    "url": "/favicon-32x32.png"
+    "url": "./favicon-32x32.png"
   },
   {
     "revision": "ab7c6d98c87a97f75bec0cea01748f9e",
-    "url": "/favicon-64x64.png"
+    "url": "./favicon-64x64.png"
   },
   {
     "revision": "af9f4111591f97625208fa67507eaf51",
-    "url": "/img/screenshot/screenshot-1.webp"
+    "url": "./img/screenshot/screenshot-1.webp"
   },
   {
     "revision": "2d834cacfadb06009fbd02079b77c190",
-    "url": "/img/screenshot/screenshot-2.webp"
+    "url": "./img/screenshot/screenshot-2.webp"
   },
   {
     "revision": "fd87245436f9d400e03dd8f054bf5c3e",
-    "url": "/img/screenshot/screenshot-3.webp"
+    "url": "./img/screenshot/screenshot-3.webp"
   },
   {
     "revision": "3fa864f1766aad3150f01eebf169f20f",
-    "url": "/index.html"
+    "url": "./index.html"
   },
   {
     "revision": "9ba644d1fac3d70306a1",
-    "url": "/js/app.960a96b5.js"
+    "url": "./js/app.960a96b5.js"
   },
   {
     "revision": "edba500b29f4c690a071",
-    "url": "/js/chunk-vendors.32557857.js"
+    "url": "./js/chunk-vendors.32557857.js"
   },
   {
     "revision": "593e45764157a48c24618698919fd626",
-    "url": "/manifest.json"
+    "url": "./manifest.json"
   },
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
-    "url": "/robots.txt"
+    "url": "./robots.txt"
   },
   {
     "revision": "132c77b8ae27d3327dd86d35e67fed69",
-    "url": "/safari-pinned-tab.svg"
+    "url": "./safari-pinned-tab.svg"
   },
   {
     "revision": "849d786c115e1081d313783026ae2602",
-    "url": "/service-worker.js"
+    "url": "./service-worker.js"
   }
 ]);
