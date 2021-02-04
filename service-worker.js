fbf9e26007e7814297f9e66e4968473d223d1b38
fmented <fmented@gmail.com>
2021-02-04 15:30:10 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -14,10 +14,10 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9cd20cc56abb0cdadd94adaccc80f298.js"
+  "./precache-manifest.9cd20cc56abb0cdadd94adaccc80f298.js"
 );
 
-workbox.core.setCacheNameDetails({prefix: "manifest-generator"});
+workbox.core.setCacheNameDetails({prefix: "web-essential"});
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
